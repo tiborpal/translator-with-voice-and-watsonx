89d7eab0ea5b7f361050d08b6da9b1642dbe7d2d
tiborpal <tiborpal76@gmail.com>
2025-02-22 15:59:46 +0100

    Adding Watsonx specifix parameters etc.

--- worker.py
+++ worker.py
@@ -1,3 +1,38 @@
+# To call watsonx's LLM, we need to import the library of IBM Watson Machine Learning
+from ibm_watson_machine_learning.foundation_models.utils.enums import ModelTypes
+from ibm_watson_machine_learning.foundation_models import Model
+
+# placeholder for Watsonx_API and Project_id incase you need to use the code outside this environment
+# API_KEY = "Your WatsonX API"
+PROJECT_ID= "skills-network"
+
+# Define the credentials 
+credentials = {
+    "url": "https://us-south.ml.cloud.ibm.com"
+    #"apikey": API_KEY
+}
+    
+# Specify model_id that will be used for inferencing
+model_id = ModelTypes.FLAN_UL2
+
+# Define the model parameters
+from ibm_watson_machine_learning.metanames import GenTextParamsMetaNames as GenParams
+from ibm_watson_machine_learning.foundation_models.utils.enums import DecodingMethods
+
+parameters = {
+    GenParams.DECODING_METHOD: DecodingMethods.GREEDY,
+    GenParams.MIN_NEW_TOKENS: 1,
+    GenParams.MAX_NEW_TOKENS: 1024
+}
+
+# Define the LLM
+model = Model(
+    model_id=model_id,
+    params=parameters,
+    credentials=credentials,
+    project_id=PROJECT_ID
+)
+
 def speech_to_text(audio_binary):
     return None
 
